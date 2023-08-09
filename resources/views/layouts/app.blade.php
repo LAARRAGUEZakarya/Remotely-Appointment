604cfcb7bf46e117583440ff7af1db502bde61b6
zakarya <zakariya.laarrague@outlook.fr>
2023-08-09 16:34:04 +0100

    mkdir style.css and main.js  in public folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,11 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="csrf-token" content="{{ csrf_token() }}">
                 <!-- Favicon-->
-                <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-                <!-- Core theme CSS (includes Bootstrap)-->
-                <link href="../views/css/styles.css" rel="stylesheet" />
+                <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+               
+
+                {{-- main of css --}}
+                <link rel="stylesheet" href="{{ asset('css/style.css') }}">
                 {{-- css of bootstrap --}}
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
          
@@ -27,6 +29,6 @@
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
           
                 {{-- this js script it's for welcom page --}}
-                <script src="../views/js/scripts.js"></script>
+                <script src="{{asset('js/main.js')}}"></script>
             </body>
         </html>
