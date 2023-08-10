e49d21500f3a22a82f8c19f3ab0ce14c38856ce7
zkaria <zakariya.laarrague@outlook.fr>
2023-08-10 23:13:27 +0100

    delete name fields from register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,12 +6,7 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+
                <!-- first name -->
         <div>
              <x-input-label for="first_name" :value="__('First name')" />
