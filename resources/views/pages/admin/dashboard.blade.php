46a250dc035898454f9555d29ec86224e879d587
zakarya <zakariya.laarrague@outlook.fr>
2023-08-10 10:23:48 +0100

    add dashboard client and dashboard admin

---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title')Dashboard Admin @endsection
+@section('content')
+
+    <h1>BRO YOU ARE IN <span class="span_dashboard">ADMIN</span> DASHBOARD</h1>
+  
+@endsection
---
+++ resources/views/pages/client/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title')Dashboard Client @endsection
+@section('content')
+
+    <h1>BRO YOU ARE IN <span class="span_dashboard">CLIENT</span> DASHBOARD</h1>
+  
+@endsection
